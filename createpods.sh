f792281542c82f6ee2984cc39de9f70eab25cf1b
riowiraldhani <53550154+riowiraldhani@users.noreply.github.com>
2020-12-07 22:30:38 +0700

    Create createpods.sh

---
+++ createpods.sh
@@ -0,0 +1,4 @@
+kubectl create deployment helloapp-1 --image=gcr.io/google-samples/hello-app:1.0
+kubectl create deployment helloapp-2 --image=gcr.io/google-samples/hello-app:2.0
+kubectl create deployment httpdku --image=httpd
+kubectl get deployment
